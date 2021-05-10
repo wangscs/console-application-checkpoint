3086fceaa65189183d3b59644dd7032c4c5075e8
Okinz <steven14812@gmail.com>
2021-05-10 16:53:29 -0400

    Added checkpoint manipulation process

---
+++ NumberManipulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var myInt = 12;
+      Console.WriteLine(myInt);
+      myInt = (myInt * myInt) + 14 - 3;
+      Console.WriteLine(myInt);
+    }
+  }
+}
